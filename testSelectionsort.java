59e011301c812b69034ef54d8539c871f21c80f4
Gabriel Gonzales <gabgonz@umich.edu>
2022-04-14 14:43:11 -0400

    Added testPositive() and testNegative().

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,3 +13,42 @@ testDuplicates();
 }
     public testSelectionSort() {
     }
+
+
+    public void testPositive(){
+        int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        arr[4] = 2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+
+        assert(Sortedarr[1] < Sortedarr[2]): "Assertion Error!";
+        System.out.println(Arrays.toString(Sortedarr));
+}
+
+    public void testNegative(){
+    	 int[] arr = new int[5];
+         arr[0] = -8;
+         arr[1] = -9;
+         arr[2] = -7;
+         arr[3] = -10;
+         arr[4] = -2;
+
+         int[] Sortedarr = new int[5];
+         Sortedarr[0] = -10;
+         Sortedarr[1] = -9;
+         Sortedarr[2] = -8;
+         Sortedarr[3] = -7;
+         Sortedarr[4] = -2;
+
+         assert(Sortedarr[1] < Sortedarr[2]): "Assertion Error!";
+         System.out.println(Arrays.toString(Sortedarr));
+}
